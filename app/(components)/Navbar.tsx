2559ab3bec5fb3f474df8a27ceb1f5ca9d66f883
harsh1924 <harshrpanwar@gmail.com>
2025-07-10 15:58:40 +0530

    Added Yoga Page

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -9,9 +9,15 @@ const Navbar = () => {
         <nav className="w-full px-6 py-4 flex items-center justify-between border-b border-yellow-200">
             <Link
                 href={'/'}
-                className="text-2xl font-bold text-[#ce9a48] tracking-wider"
+                className="text-2xl font-bold text-[#ce9a48] tracking-wider hidden md:flex"
             >
                 MatchMade in Rashi
+            </Link>
+            <Link
+                href={'/'}
+                className="text-2xl font-bold text-[#ce9a48] tracking-wider md:hidden"
+            >
+                MMR
             </Link>
             {/* Optional Nav Items */}
             <div className="space-x-4">
@@ -26,6 +32,12 @@ const Navbar = () => {
                     className='cursor-pointer bg-[#ce9a48] hover:bg-[#5f4e33] transition-all duration-300'
                 >
                     Match
+                </Button>
+                <Button
+                    onClick={() => router.push("/yogas")}
+                    className='cursor-pointer bg-[#ce9a48] hover:bg-[#5f4e33] transition-all duration-300'
+                >
+                    Yogas
                 </Button>
             </div>
         </nav>
---
+++ app/yogas/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import MajorYogas from '../(components)/MajorYogas'
+import Navbar from '../(components)/Navbar'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar />
+        <MajorYogas />
+    </div>
+  )
+}
+
+export default page
